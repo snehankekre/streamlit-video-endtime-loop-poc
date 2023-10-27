47c8bd99a9ec10260312dde3ff11b19d49c7f123
snehankekre <snehan.minerva@gmail.com>
2023-10-27 17:07:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# POC: `st.video` and `st.audio` with end time and looping
+
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://media-endtime-loop-poc.streamlit.app)
---
+++ app.py
@@ -0,0 +1,20 @@
+import numpy as np
+import streamlit as st
+
+VIDEO_URL = "https://static.streamlit.io/examples/star.mp4"
+
+col1, col2 = st.columns(2)
+start_time = col1.slider("Start time", 0, 10, 0)
+end_time = col2.slider("End time", 0, 10, 10)
+loop = st.toggle("Loop media", True)
+
+st.video(VIDEO_URL, start_time=start_time, end_time=end_time, loop=loop)
+
+
+sample_rate = 44100
+seconds = 10
+frequency_la = 440
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+AUDIO_URL = np.sin(frequency_la * t * 2 * np.pi)
+
+st.audio(AUDIO_URL, sample_rate=sample_rate, start_time=start_time, end_time=end_time, loop=loop)
