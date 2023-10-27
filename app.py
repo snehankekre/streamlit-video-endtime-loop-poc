eec3a8b73ed334fa39f068e6138c0a6ef158de18
snehankekre <snehan.minerva@gmail.com>
2023-10-27 17:13:13 +0530

    Update end time of media

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import streamlit as st
 VIDEO_URL = "https://static.streamlit.io/examples/star.mp4"
 
 col1, col2 = st.columns(2)
-start_time = col1.slider("Start time", 0, 10, 0)
-end_time = col2.slider("End time", 0, 10, 10)
+start_time = col1.slider("Start time", 0, 5, 0)
+end_time = col2.slider("End time", 0, 5, 5)
 loop = st.toggle("Loop media", True)
 
 st.video(VIDEO_URL, start_time=start_time, end_time=end_time, loop=loop)
 
 
 sample_rate = 44100
-seconds = 10
+seconds = 5
 frequency_la = 440
 t = np.linspace(0, seconds, seconds * sample_rate, False)
 AUDIO_URL = np.sin(frequency_la * t * 2 * np.pi)
